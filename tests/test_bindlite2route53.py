2d29daa28357f1bd924d4e0ac08e723be3d50e8c
Nicholas Fowler <nbfowler@gmail.com>
2016-04-05 16:28:49 -0700

    update test_bindlite2route53.py

--- tests/test_bindlite2route53.py
+++ tests/test_bindlite2route53.py
@@ -4,15 +4,15 @@ from moto import mock_route53
 from StringIO import StringIO
 
 bad_bindlites = """
-macadamianuts.fyre.co.     CNAMEwww.livefyre.com
-nicho.fyre.co.      CNAME www.livefyre.com
-noearzcat.fyre.co.CNAME    www.livefyre.com
+macadamianuts     CNAMEwww.livefyre.com
+nicho      CNAME www.livefyre.com
+noearzcatCNAME    www.livefyre.com
 """
 
 good_bindlites = """
-macadamianuts.fyre.co. CNAME www.livefyre.com
-nicho.fyre.co. CNAME www.livefyre.com
-noearzcat.fyre.co. CNAME www.livefyre.com
+macadamianuts CNAME www.livefyre.com
+nicho CNAME www.livefyre.com
+noearzcat CNAME www.livefyre.com
 """
 
 good_r53 = [{'ResourceRecords': ['www.livefyre.com'], 'Type': 'CNAME', 'Name': 'macadamianuts.fyre.co.', 'TTL': '3600'}, {'ResourceRecords': ['www.livefyre.com'], 'Type': 'CNAME', 'Name': 'nicho.fyre.co.', 'TTL': '3600'}, {'ResourceRecords': ['www.livefyre.com'], 'Type': 'CNAME', 'Name': 'noearzcat.fyre.co.', 'TTL': '3600'}]
@@ -22,13 +22,14 @@ class TestBindlite2Route53():
     def setUp(self):
         self.good_file = StringIO(good_bindlites.strip())
         self.bad_file = StringIO(bad_bindlites.strip())
+        self.zone = 'fyre.co'
 
     def test_bindlite2route53(self):
-        good_records = bindlite2route53(self.good_file)
+        good_records = bindlite2route53(self.good_file, self.zone)
         assert isinstance(good_records, list)
         assert good_r53 == good_records
 
-        assert_raises(SystemExit, bindlite2route53, self.bad_file)
+        assert_raises(ValueError, bindlite2route53, self.bad_file, self.zone)
 
     def tearDown(self):
         self.good_file.close()
