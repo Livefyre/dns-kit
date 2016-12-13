581ef2b6e2c6f26fbe6d23b48759112b2fa25497
Nicholas Fowler <nbfowler@gmail.com>
2016-12-12 16:44:53 -0800

    sort ResourceRecords before doing r53 diff

--- dns_kit/route53diff.py
+++ dns_kit/route53diff.py
@@ -18,14 +18,25 @@ def stamp_change(records, change):
         change_dict = {'Action': change, 'Record': record_dict}
         yield json.dumps(change_dict) + "\n"
 
+def sort_resource_records(r53_file):
+    sorted_output = []
+    for obj in r53_file.readlines():
+        _dict = json.loads(obj)
+        _dict['ResourceRecords'] = sorted(_dict['ResourceRecords'])
+        sorted_output.append(json.dumps(_dict))
+
+    return sorted_output
+
+
 def main():
     args = docopt(usage)
     current_file = open(args['<current>'], 'r')
     desired_file = open(args['<desired>'], 'r')
 
+
     # assumes input files are ordered
-    delta = difflib.ndiff(current_file.readlines(),
-                desired_file.readlines())
+    delta = difflib.ndiff(sort_resource_records(current_file),
+                sort_resource_records(desired_file))
 
     delta = list(delta)
     to_delete = [line.replace('- ','') for line in delta if line.startswith('-')]
